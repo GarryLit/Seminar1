e0824b946cc697cce97fb576d83588817f192475
GarryLit <garry_lit@mail.ru>
2022-05-13 00:26:58 +0300

    1

---
+++ S1T2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 2: Напишите программу, которая принимает на
+вход три числа и выдаёт максимальное из этих чисел.
+*/
+
+Console.Write("Введите первое число  ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число  ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число  ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+// Классический алгоритм
+
+if(numberA > numberB)
+    if(numberA > numberC)
+        Console.Write(numberA); 
+    else
+        if(numberB > numberC)
+            Console.Write(numberC);
+        else    
+            Console.Write(numberC);
+else
+    if(numberB > numberC)
+        Console.Write(numberB);    
+    else    
+        Console.Write(numberC);
+
+//C  третьей переменной
+/*
+int max = numberA;
+if(numberB > max)
+    max = numberB;
+if(numberC > max)
+    max = numberC;
+
+Console.Write(max); 
+*/
---
+++ S1T3/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 3: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+*/
+
+Console.Write("Введите число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 2;
+if(result != 0) 
+    Console.Write("Не четное");
+else
+    Console.Write("Четное");    
+
